ca41a630d97daaac22c9c9ad0d213e4874816be6
Andres Villalobose <aevr007@gmail.com>
2018-11-29 09:48:43 -0300

    Adding new ENC structure and database.sql

---
+++ database.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS Nodes;
+CREATE TABLE "Nodes" (
+	"node_definition"  TEXT UNIQUE NOT NULL Primary Key,
+	"cluster"  TEXT NOT NULL,
+	"role"  TEXT NOT NULL,
+	"environment"  TEXT NOT NULL
+);
+.separator ","
+.import config/nodes_database.csv Nodes
+DROP TABLE IF EXISTS TemporaryEnvironmentConfiguration;
+CREATE TABLE "TemporaryEnvironmentConfiguration" (
+	"fqdn"  TEXT UNIQUE NOT NULL Primary Key,
+	"environment"  TEXT NOT NULL,
+	"ConfTimeStart"  TEXT NOT NULL,
+	"ConfTimeEnd"  TEXT NOT NULL
+);
+.separator ","
+.import config/environments_database.csv TemporaryEnvironmentConfiguration
